57919dd39202e48bc5cac1d614443ec753eef261
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-08-08 03:01:00 +0100

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -271,6 +271,7 @@ cmd(
                     }
                 ];
                 for (let i=0; i < categories.length; i++) {
+                    if (categories[i].toLowerCase() === "misc") continue
                     let cat = global.catInfo[categories[i].toUpperCase()];
                     let command = cat?.pattern || categories[i].toLowerCase() + "menu"
                     let row = { title: `${i + 1}`, rowId: `${prefix}${command}`, description: categories[i].charAt(0).toUpperCase() + categories[i].slice(1) + " Commands"}
