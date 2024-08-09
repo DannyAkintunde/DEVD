3fc1320676e3f8b595556a94243f0ec7a02073c7
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-08-09 18:12:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -97,27 +97,27 @@ async function connectToWA() {
         logger: P({ level: "fatal" }).child({ level: "fatal" }),
         printQRInTerminal: true,
         browser: Browsers.windows('Desktop'),
-        patchMessageBeforeSending: (message) => {
-            const requiresPatch = !!(
-                message.buttonsMessage ||
-                message.templateMessage ||
-                message.listMessage
-            );
-            if (requiresPatch) {
-                message = {
-                    viewOnceMessage: {
-                        message: {
-                            messageContextInfo: {
-                                deviceListMetadataVersion: 2,
+      //  patchMessageBeforeSending: (message) => {
+       //     const requiresPatch = !!(
+         //       message.buttonsMessage ||
+         //       message.templateMessage ||
+      //          message.listMessage
+       //     );
+      //      if (requiresPatch) {
+       //         message = {
+     //               viewOnceMessage: {
+       //                 message: {
+         //                   messageContextInfo: {
+        //                        deviceListMetadataVersion: 2,
                                 deviceListMetadata: {},
-                            },
-                            ...message,
-                        },
-                    },
-                };
-            }
-            return message;
-        },
+        //                    },
+      //                      ...message,
+      //                  },
+        //           },
+         //       };
+         //   }
+        //    return message;
+    //    },
         syncFullHistory: true,
         generateHighQualityLinkPreview: true,
         auth: {
