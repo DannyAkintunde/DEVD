0a22778d91a558881bae90ef458ec79e85a15dea
DannyAkintunde <doakintinde@outlook.com>
2024-08-27 16:53:09 +0100

    f

--- plugins/test.js
+++ plugins/test.js
@@ -128,7 +128,51 @@ cmd(
                 return await conn.replyList(from, listMessage, { quoted: msg });
             }
         } catch (e) {
-         m.sendError(e)
+            m.sendError(e);
         }
     }
 );
+
+cmd({ pattern: "ttf" }, async (conn, mek, m, opt) => {
+    let x = {
+        viewOnceMessage: {
+            message: {
+                interactiveMessage: {
+                    contextInfo: {
+                        mentionedJid: [m.sender],
+                        isForwarded: true,
+                        forwardedNewsletterMessageInfo: {
+                            newsletterJid: "0@newsletter",
+                            newsletterName: global.responses.by,
+                            serverMessageId: 1
+                        }
+                    },
+                    header: {
+                        title: "header"
+                    },
+                    body: {
+                        text: "body"
+                    },
+                    footer: {
+                        text: "⿻ fotter ⿻"
+                    },
+                    nativeFlowMessage: {
+                        buttons: [
+                            {
+                                name: "single_select",
+                                buttonParamsJson: `{ "title": "⿻Kyoja+⿻", "sections": [{ "title": "# !-Choose One Of Them", "highlight_label": "🌏General Commands🗨️", "rows": [{ "header": "ALL COMMAND", "title": "Show All Command", "id": ".allmenu" }, { "header": "Owner", "title": "Displays Owner Number", "id": ".owner" }, { "header": "Bot Info", "title": "Displays Information About Bots", "id": ".botstatus" }] }, { "title": "🦠 SpeCiaL - ComManD ❌", "highlight_label": " #SpeCial ", "rows": [{ "header": "Special - Menu", "title": "displays all special commands", "id": ".spesialmenu" }] }] }`
+                            },
+                            {
+                                name: "cta_url",
+                                buttonParamsJson:
+                                    '{"display_text":"Saluran WhatsApp","url":"https://whatsapp.com/channel/0029VadBczKI1rcayqzQ2n0e","merchant_url":"https://whatsapp.com/channel/0029VadBczKI1rcayqzQ2n0e"}'
+                            }
+                        ],
+                        messageParamsJson: ""
+                    }
+                }
+            }
+        }
+    };
+    conn.relayMessage(opt.from, x, {});
+});
