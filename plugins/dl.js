3e81c2596f04049edde8686f5e6f424415563ec2
DannyAkintunde <dannyakintundelct@gmail.com>
2025-02-05 10:27:07 +0100

    update

--- plugins/dl.js
+++ plugins/dl.js
@@ -159,7 +159,7 @@ cmd(
             const gfile = await conn.sendMessage(
                 from,
                 {
-                    document: getBuffer(data.downloadUrl),
+                    document: await getBuffer(data.downloadUrl),
                     fileName: data.fileName,
                     mimetype: mimes.lookup(data.fileName),
                     jpegThumbnail: await getBuffer(
